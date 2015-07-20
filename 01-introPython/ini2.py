5567037ddf12344f4f20d8747e86e0d557a94ec9
danisetiawan <danisetiawan@users.noreply.github.com>
2015-07-20 05:13:29 +0000

    first commit

---
+++ 01-introPython/ini2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+a = int(raw_input("Input the first triangle's leg length: "))
+b = int(raw_input("Input the second triangle's leg length: "))
+c = (a**2)+(b**2)
+
+print "The square of the length of the hypotenuse is: ", c
+
